5ab873319aa06cb36043a5b8c1d99aee9d8dbc8a
Emmanuel Carbonell <emmanuel.carbonell.lopez1704@gmail.com>
2022-05-06 17:07:03 -0400

    algo ahi

---
+++ WebAppPract/Controllers/EstudianteController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppPract.Models;
+using System.IO;
+
+
+//Laalalalalaalla
+
+
+namespace WebAppPract.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        // GET: Estudiante
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        public ActionResult Resultados(Estudiante estudi, HttpPostedFileBase foto)
+        {
+            string nombreFoto = Path.GetFileName(foto.FileName);
+
+            string rutaFoto = Path.Combine(Server.MapPath("/Images"), nombreFoto);
+
+            foto.SaveAs(rutaFoto);
+
+            ViewBag.picture = nombreFoto;
+
+          
+            return View(estudi);
+        }
+
+       
+        
+        
+        public ActionResult ProcesaFoto(string foto)
+        {
+            string ruta = ControllerContext.HttpContext.Server.MapPath("/Ïmages");
+
+
+            return File(Path.Combine(ruta, foto) ,"image");
+        }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+
+}
---
+++ WebAppPract/Models/Estudiante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppPract.Models
+{
+    public class Estudiante
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+        public string Genero { get; set; }
+
+        public string EstadoCivil { get; set; }
+
+        public string Hobby { get; set; }
+
+        public string Foto { get; set; }
+
+    }
+}
